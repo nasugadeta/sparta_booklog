219b3a67986686df1a2b2583ed21a96614b2c719
Hirofumi Matsunaga <hiromatsu0707@gmail.com>
2015-06-27 13:16:06 +0900

    show画面に、レビュー投稿者のemailなどを表示する機能を実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,21 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(
+      book_id: params[:book_id],
+      user_id: current_user.id,
+      body:    review_params["body"]
+      )
+    @review.save
+    redirect_to book_url(@review.book)
+  end
+
   def new
     @book = Book.find(params[:book_id])
+    @review = Review.new
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:body)
   end
 end
